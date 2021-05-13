3abccc9733a722e37e02ef0231cd10b4b5f091a5
Jonathan Rehem <jonathan.de.oliveira@live.com>
2021-05-13 09:23:38 -0300

    header responsivity

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,21 +1,26 @@
 import { Flex, Box, Avatar, Text } from "@chakra-ui/react";
 import React from "react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
     return (
         <Flex align="center">
-            <Box
-                mr="4"
-                textAlign="right"
-            >
-                <Text>Jonathan Rehem</Text>
-                <Text
-                    color="gray.300"
-                    fontSize="small"
+            {showProfileData && (
+                <Box
+                    mr="4"
+                    textAlign="right"
                 >
-                    rehemdev@gmail.com
-            </Text>
-            </Box>
+                    <Text>Jonathan Rehem</Text>
+                    <Text
+                        color="gray.300"
+                        fontSize="small"
+                    >
+                        rehemdev@gmail.com
+                    </Text>
+                </Box>)}
 
             <Avatar size="md" name="name" src="https://github.com/90srehem.png" />
         </Flex>
